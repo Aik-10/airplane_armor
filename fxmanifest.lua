de5759d42fdc2c7dbd0ad55c0e365917e921a431
Lentokone <44865524+Aik-10@users.noreply.github.com>
2020-10-26 19:23:37 +0200

    Master

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts { "**/cl_*.lua" }
+server_scripts { "@mysql-async/lib/MySQL.lua", "**/sv_*.lua" }
+
+author 'Lentokone'
+version '1.0.0'
+
+-- DumpProof Fuck YEAH BY Lentokone
+-- Vitun Mikkihiiri mitä vittua sä koitat dumptata!?!?!?
+-- DISCORD <@228494142236393472> Voin antaa sulle saatana koko paskan!
+-- VITUTAAKO HÄ VITUTTAAKO PAINU HELVETISTÄ VITTUUN
